992298ce713dc9de9a75630cb9d636684ff538a3
abderrahmaneGasmi <abderrahmen.gasmi@univ-constantine2.dz>
2024-05-16 13:27:29 +0100

    add filter products

--- app/(home)/products.tsx
+++ app/(home)/products.tsx
@@ -1,4 +1,5 @@
 import {
+  FlatList,
   Image,
   ScrollView,
   StyleSheet,
@@ -6,133 +7,183 @@ import {
   TextInput,
   View,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { normalize, typography } from "@/constants/typography";
 import { colors } from "@/constants/Colors";
 import Productitem from "@/components/Productitem";
-import { Product } from "@/constants/types";
-import { getProduct } from "@/api/products";
+import { Product, category } from "@/constants/types";
+import { filterProducts, getProduct } from "@/api/products";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const products = () => {
   const finalproducts = React.useRef([] as Product[]);
   const [productsrendered, setProductsrendered] = useState([] as Product[]);
+  const [filter, setFilter] = useState("All" as "All" | category);
   React.useEffect(() => {
     getProduct({ limit: 50 }).then((data) => {
       finalproducts.current = data;
       setProductsrendered(data);
     });
   }, []);
+  const categories = [
+    "All",
+    "electronics",
+    "jewelery",
+    "men's clothing",
+
+    "women's clothing",
+  ];
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  // callbacks
+  const handleSheetChanges = useCallback((index: number) => {
+    // console.log("handleSheetChanges", index);
+  }, []);
   return (
-    <SafeAreaView>
-      <ScrollView
-        contentContainerStyle={{
-          height: "100%",
-        }}
-      >
-        <View style={styles.container}>
-          <View style={styles.header}>
-            <View></View>
-            <View></View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView>
+        <ScrollView
+          contentContainerStyle={{
+            height: "100%",
+          }}
+        >
+          <View style={styles.container}>
+            <View>
+              <View style={styles.header}>
+                <View></View>
+                <View></View>
 
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                gap: 10,
-              }}
-            >
-              <Image
-                source={require("@/assets/images/logo.png")}
-                style={{
-                  width: 40,
-                  height: 40,
-                  resizeMode: "contain",
-                }}
-              />
-
-              <Text style={styles.logotext}>EasydooTest</Text>
-            </View>
-            <View></View>
-
-            <Ionicons name="person-circle" size={24} color={colors.darkgray} />
-          </View>
-          <View style={styles.search}>
-            <Ionicons name="search" size={24} color={colors.mediumgray} />
-            <TextInput
-              style={{
-                marginLeft: 10,
-                color: colors.darkgray,
-                // width: "100%",
-              }}
-              placeholder="Search for products"
-            />
-          </View>
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              alignItems: "center",
-              marginBottom: normalize(10),
-            }}
-          >
-            <Text
-              style={{
-                fontSize: typography.medium,
-                color: colors.black,
-                fontWeight: "bold",
-              }}
-            >
-              50 Product
-            </Text>
-            <View style={styles.btns}>
-              <View style={styles.btn}>
-                <Text
+                <View
                   style={{
-                    color: colors.darkgray,
-                    fontSize: typography.small,
+                    flexDirection: "row",
+                    alignItems: "center",
+                    gap: 10,
                   }}
                 >
-                  Filter
-                </Text>
+                  <Image
+                    source={require("@/assets/images/logo.png")}
+                    style={{
+                      width: 40,
+                      height: 40,
+                      resizeMode: "contain",
+                    }}
+                  />
+
+                  <Text style={styles.logotext}>EasydooTest</Text>
+                </View>
+                <View></View>
+
                 <Ionicons
-                  name="funnel-outline"
-                  size={20}
+                  name="person-circle"
+                  size={24}
                   color={colors.darkgray}
                 />
               </View>
+              <View style={styles.search}>
+                <Ionicons name="search" size={24} color={colors.mediumgray} />
+                <TextInput
+                  style={{
+                    marginLeft: 10,
+                    color: colors.darkgray,
+                    // width: "100%",
+                  }}
+                  placeholder="Search for products"
+                />
+              </View>
+              <FlatList
+                horizontal
+                data={categories}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item }) => (
+                  <View
+                    style={{
+                      padding: 10,
+                      backgroundColor:
+                        filter === item ? colors.primary : "white",
+
+                      borderRadius: 10,
+                      marginHorizontal: 5,
+                      alignSelf: "flex-start",
+                    }}
+                    onTouchEnd={() => {
+                      setFilter(item as "All" | category);
+                      if (item === "All") {
+                        setProductsrendered(finalproducts.current);
+                      } else {
+                        filterProducts(item as category).then((data) => {
+                          setProductsrendered(data);
+                        });
+                      }
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: filter === item ? colors.white : colors.darkgray,
+                        fontSize: typography.small,
+                      }}
+                    >
+                      {item}
+                    </Text>
+                  </View>
+                )}
+                contentContainerStyle={
+                  {
+                    // marginBottom: 20,
+                  }
+                }
+                style={
+                  {
+                    // marginBottom: 20,
+                  }
+                }
+              />
+            </View>
+            <View>
+              <ScrollView horizontal={false}>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    // marginVertical: 20,
+                    // marginBottom: 70,
+                    flexWrap: "wrap",
+                    justifyContent: "space-between",
+                    width: "100%",
+                    gap: 10,
+                    flex: 1,
+                  }}
+                >
+                  {productsrendered.map((product, index) => {
+                    return (
+                      <Productitem
+                        key={index}
+                        title={product.title}
+                        price={product.price}
+                        category={product.category}
+                        image={product.image}
+                      />
+                    );
+                  })}
+                </View>
+              </ScrollView>
             </View>
           </View>
-          <ScrollView horizontal={false}>
-            <View
-              style={{
-                flexDirection: "row",
-                // marginVertical: 20,
-                // marginBottom: 70,
-                flexWrap: "wrap",
-                justifyContent: "space-between",
-                width: "100%",
-                gap: 10,
-                flex: 1,
-              }}
-            >
-              {productsrendered.map((product, index) => {
-                return (
-                  <Productitem
-                    key={index}
-                    title={product.title}
-                    price={product.price}
-                    category={product.category}
-                    image={product.image}
-                  />
-                );
-              })}
-            </View>
-          </ScrollView>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+        </ScrollView>
+      </SafeAreaView>
+      <BottomSheet
+        ref={bottomSheetRef}
+        onChange={handleSheetChanges}
+        snapPoints={[300]}
+        enablePanDownToClose={true}
+        index={-1}
+      >
+        <BottomSheetView style={styles.contentContainer}>
+          <Text>Awesome 🎉</Text>
+        </BottomSheetView>
+      </BottomSheet>
+    </GestureHandlerRootView>
   );
 };
 
@@ -143,6 +194,7 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingVertical: normalize(10),
     paddingHorizontal: normalize(12),
+    gap: 20,
   },
   header: {
     flexDirection: "row",
@@ -187,5 +239,9 @@ const styles = StyleSheet.create({
     elevation: 5,
     borderRadius: 10,
     backgroundColor: colors.white,
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: "center",
   },
 });
